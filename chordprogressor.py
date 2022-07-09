709728ea58df554132d0e487e76b48d059c46938
koirves <dermiitti@gmail.com>
2022-07-09 05:39:03 +0300

    Update chordprogressor.py
    Plots each chord in progression. Note intervals need rechecking.

--- chordprogressor.py
+++ chordprogressor.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+
 """
 Produces simple chord progression for minor and major scales by key.
 8-JUN-2022 / Vesa Koiramäki
@@ -54,7 +55,7 @@ scaleType = None
 while(scaleType not in ["M", "m"]):
     scaleType = input("Do you want the progression to be major (M) or minor (m) scale? [M/m]: ")
 
-
+chordType = None
 scale = []
 print(key)
 idx = noteNotations.index(key)
@@ -64,12 +65,14 @@ if(scaleType == "M"):
         note = noteNotations[idx%len(noteNotations)]
         scale.append(notes[note])
         degreeMajor = majorDegreeMajor
+        chordType = "Major scale"
 elif(scaleType == "m"):
     for progressionStep in minorScale:
         idx = idx + progressionStep
         note = noteNotations[idx%len(noteNotations)]
         scale.append(notes[note])
         degreeMajor = minorDegreeMajor
+        chordType = "Minor scale"
 
 chordProgression = []
 for idx, freq in enumerate(scale[0:7]):
@@ -94,6 +97,42 @@ print(t)
 progression = input("Insert a serie of digits between 1 to 7 separated by comma, i.e. 1, 7, 6, 7: ")
 progression = progression.strip().split(",")
 
+
+
+fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6), (ax7, ax8)) = plt.subplots(4, 2)
+fig.suptitle(f"{chordType} chord progression for {key}-key")
+plots = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8]
+length = 250
+
+
+for idx, chord in enumerate(chordProgression):
+
+    freq = chords[chord]
+ 
+    signal1 = amp * np.sin(2.0 * np.pi * freq[0] * t)
+    signal2 = amp * np.sin(2.0 * np.pi * freq[1] * t)
+    signal3 = amp * np.sin(2.0 * np.pi * freq[2] * t)
+
+    signal = signal1 + signal2 + signal3
+
+    plots[idx].plot(signal1[:int(length)], label=list(notes.keys())[list(notes.values()).index(freq[0])])
+    plots[idx].plot(signal2[:int(length)], label=list(notes.keys())[list(notes.values()).index(freq[1])])
+    plots[idx].plot(signal3[:int(length)], label=list(notes.keys())[list(notes.values()).index(freq[2])])
+    plots[idx].plot(signal[:int(length)], label=chord)
+    plots[7].plot(signal[:int(length)], label=chord)
+
+
+
+for idx, ax in enumerate(fig.get_axes()):
+    ax.label_outer()
+    ax.legend()
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Amplitude")
+    if(idx < 7):
+        ax.set_title(chordProgression[idx])
+    else:
+        ax.set_title("All chords")
+
 for number in progression:
     print(progression, number)
     chord = chordProgression[int(number)-1]
@@ -106,13 +145,6 @@ for number in progression:
 
     signal = signal1 + signal2 + signal3
 
-#print(signal)
-
-    plt.plot(signal1[:int(samplerate/freq[0])])
-    plt.plot(signal2[:int(samplerate/freq[1])])
-    plt.plot(signal3[:int(samplerate/freq[2])])
-    plt.plot(signal[:int(samplerate/freq[0])])
-    plt.show()
-
     sd.play(signal, samplerate)
     time.sleep(dur)
+plt.show()
